99d1062f39d10a95d6e1ab2cc9712c44d9efe200
Ritik Singh <ritik.singh@spit.ac.in>
2020-07-17 18:21:21 +0530

    set debug false

--- cexit/settings.py
+++ cexit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG_VALUE']
+DEBUG = (os.environ['DEBUG_VALUE'] == True)
 
 ALLOWED_HOSTS = ['localhost','courseexit.herokuapp.com']
 
